7d3737b7c9dfd066147d89a17b687fffd3713804
ShellyBrady <mchellebr@hotmail.com>
2023-02-24 17:03:50 +0000

    reinstall everything that disappeared after shut down error. add css file and template folder

--- recipes/admin.py
+++ recipes/admin.py
@@ -8,9 +8,10 @@ class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
+    list_filter = ['status', 'author']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
