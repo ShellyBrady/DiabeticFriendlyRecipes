d72ac1d8f98260c59bcca99bfb67eb91e74da6ba
ShellyBrady <mchellebr@hotmail.com>
2023-02-24 22:38:52 +0000

    add html files

--- django_recipe/urls.py
+++ django_recipe/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("recipes.urls"), name="recipes_urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('django_recipe/', include("recipes.urls"), name="recipes_urls"),
-
 ]
